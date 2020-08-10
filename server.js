b373a0adb87c8dcc58a4f61d95583dcd83edbf48
Noah Menikefs <nmenikefs@gmail.com>
2020-08-10 15:52:49 -0400

    push

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const departments = require('./controllers/departments');
 const db = knex({
 	client: 'pg',
   	connection: {
-    	host : process.env.DATABASE_URL,
+    	connectionString : process.env.DATABASE_URL,
     	ssl: true
     }
 });
